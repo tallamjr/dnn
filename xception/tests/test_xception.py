af719cdf7b03830404b5df00f8da5d46bb5072c0
Tarek Allam <t.allam.jr@gmail.com>
2020-12-24 23:21:24 +0000

    Complete version with matching parameters numbers
    Numbers now match those in the paper
    
    	modified:   model/layers.py
    	modified:   model/xception.py
    	modified:   tests/test_xception.py

--- xception/tests/test_xception.py
+++ xception/tests/test_xception.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 import tensorflow.keras.backend as K
 
 from model.xception import Xception
@@ -6,4 +7,6 @@ from model.xception import Xception
 
 def test_num_parameters():
     model = Xception()
+    inputs = tf.keras.Input(shape=[299, 299, 3])
+    model(inputs)
     assert(np.sum([K.count_params(p) for p in model.trainable_weights]) == 22855952)
